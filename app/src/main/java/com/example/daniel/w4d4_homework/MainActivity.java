3a09243776628e77cfb0111a9b738a65531b9eb6
DanielOlvera <josue_olver@yahoo.com.mx>
2016-11-11 00:49:51 -0500

    Activity transition added from main to the activitie that will contain the apps

--- app/src/main/java/com/example/daniel/w4d4_homework/MainActivity.java
+++ app/src/main/java/com/example/daniel/w4d4_homework/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.daniel.w4d4_homework;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void doVideoPlayer(View view) {
+        Intent intent = new Intent(this, VideoPlayerActivity.class);
+        startActivity(intent);
+    }
+
+    public void doMediaPlayer(View view) {
+        Intent intent = new Intent(this, MediaPlayerActivity.class);
+        startActivity(intent);
+    }
+
+    public void doForegroundSrvc(View view) {
+        Intent intent = new Intent(this, ForegroundService.class);
+        startActivity(intent);
     }
 }
