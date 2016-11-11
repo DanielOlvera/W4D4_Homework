5e9750106fd932479636c9bba9c5a3ac1ed0c5fa
DanielOlvera <josue_olver@yahoo.com.mx>
2016-11-11 17:37:57 -0500

    File Not Found exception error solved and functioning, finished that part

--- app/src/main/java/com/example/daniel/w4d4_homework/VideoPlayerActivity.java
+++ app/src/main/java/com/example/daniel/w4d4_homework/VideoPlayerActivity.java
@@ -1,13 +1,38 @@
 package com.example.daniel.w4d4_homework;
 
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class VideoPlayerActivity extends AppCompatActivity {
+
+    VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_player);
+
+        videoView = (VideoView) findViewById(R.id.mvp_videoVw);
+
+        //Creating media controller
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+
+        //media location
+        Uri uri = Uri.parse("android.resource://com.example.daniel.w4d4_homework/"+R.raw.punkiert_processing);
+        //videoView.setVideoPath("http://www.ebookfrenzy.com/android_book/movie.mp4");
+        videoView.setVideoURI(uri);
+        videoView.start();
+
     }
 }
+
+/*
+Reference:
+    http://www.imaginaformacion.com/reproduccion-de-video-desde-raw-en-android/
+    http://www.javatpoint.com/playing-video-in-android-example
+ */
