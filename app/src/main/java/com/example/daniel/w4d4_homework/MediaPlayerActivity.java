dec4ac9a50cd0f67657f17285e6cee378cdb3fca
DanielOlvera <josue_olver@yahoo.com.mx>
2016-11-13 21:15:41 -0500

    Audio Media Player finished and commited

--- app/src/main/java/com/example/daniel/w4d4_homework/MediaPlayerActivity.java
+++ app/src/main/java/com/example/daniel/w4d4_homework/MediaPlayerActivity.java
@@ -1,13 +1,39 @@
 package com.example.daniel.w4d4_homework;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MediaPlayerActivity extends AppCompatActivity {
+
+    private static final String TAG = "MediaPlayerTAG_";
+    MediaPlayer mPlayer;
+
+    Button strBtn;
+    Button stpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media_player);
+
+        strBtn = (Button) findViewById(R.id.mp_btnPlay);
+        stpBtn = (Button) findViewById(R.id.mp_btnStop);
+
+        Log.d(TAG, "onCreate: ");
+    }
+
+    public void doStart(View view) {
+        mPlayer = MediaPlayer.create(this, R.raw.withoutyou);
+        mPlayer.start();
+    }
+
+    public void doStop(View view) {
+        if(mPlayer!= null && mPlayer.isPlaying()){
+            mPlayer.stop();
+        }
     }
 }
